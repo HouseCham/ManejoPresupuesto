1baec0ab9f41a456a84b82127109d1898ecf72a0
Ramses Rmz <a00344520@tec.mx>
2022-06-10 01:23:43 -0500

    Insertar cuenta done

---
+++ ManejoPresupuesto/Servicios/RepositorioCuentas.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioCuentas
+    {
+        Task Crear(Cuenta cuenta);
+    }
+    public class RepositorioCuentas : IRepositorioCuentas
+    {
+        public readonly string connectionString;
+        public RepositorioCuentas(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Crear(Cuenta cuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(@"INSERT INTO CuentasBueno (Nombre, TipoCuentaId, Description, Balance)
+                                                        VALUES (@Nombre, @TipoCuentaId, @Descripcion, @Balance);
+                                                        SELECT SCOPE_IDENTITY();", cuenta);
+
+            cuenta.Id = id;
+        }
+    }
+}
