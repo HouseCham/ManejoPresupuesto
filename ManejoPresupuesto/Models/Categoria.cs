5e28b47b8eee67bbafd8889e9e1c3675a48f49d4
Ramses Rmz <a00344520@tec.mx>
2022-06-11 20:59:08 -0500

    Categoria index done

--- ManejoPresupuesto/Models/Categoria.cs
+++ ManejoPresupuesto/Models/Categoria.cs
@@ -5,10 +5,14 @@ namespace ManejoPresupuesto.Models
     public class Categoria
     {
         public int Id { get; set; }
+
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength: 50, ErrorMessage = "El nombre de la categoria no debe exceder los 50 caracteres")]
         public string Nombre { get; set; }
+
+        [Display(Name = "Tipo de Operacion")]
         public TipoOperacion TipoOperacionId { get; set; }
+
         public int UsuarioId { get; set; }
     }
 }
